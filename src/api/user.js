d9d2f4de10cde45c4ff7901212abbabb48aeb443
童永泽 <604232797@qq.com>
2021-01-26 17:21:40 +0800

    解决获取资料警告的问题

--- src/api/user.js
+++ src/api/user.js
@@ -12,8 +12,15 @@ export function login(data) {
   })
 }
 
-export function getInfo(token) {
-
+/**
+ *  获取用户的基本资料
+ *
+ * **/
+export function getUserInfo() {
+  return request({
+    url: '/sys/profile',
+    method: 'post'
+  })
 }
 
 export function logout() {
--- src/permission.js
+++ src/permission.js
@@ -11,14 +11,23 @@ import 'nprogress/nprogress.css' // 引入进度条样式
 // next(false) 跳转终止
 // next(地址) 跳转到某个地址
 const whiteList = ['/login', '/404'] // 定义白名单
-router.beforeEach((to, from, next) => {
+router.beforeEach(async(to, from, next) => {
   nProgress.start() // 开启进度条
   //  如果有token
   if (store.getters.token) {
+    // 只有有token 情况下 才能获取资料
+    // 如果有 token
     if (to.path === '/login') {
       // 如果要访问的是 登录页
       next('/') // 跳到主页
     } else {
+      // 只有放过的时候才去获取用户资料
+      // 如果当前vuex 中有用户的资料的 id 表示已经有资料了 不需要获取了 如果没有id才需要获取
+      if (!store.getters.userId) {
+        // 如果没有id 才表示当前用户资料没有获取过
+        await store.dispatch('user/getUserInfo')
+        // 如果说后续 需要根据用户资料获取数据的话 这里需要改成 同步
+      }
       next()
     }
   } else {
