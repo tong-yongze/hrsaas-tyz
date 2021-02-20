c56b7db53cf5c70856410701542c28c20a7dd152
童永泽 <604232797@qq.com>
2021-02-20 22:02:29 +0800

    树形内容单独抽提组件

--- src/views/departments/components/tree-tools.vue
+++ src/views/departments/components/tree-tools.vue
@@ -18,8 +18,8 @@
             <el-dropdown-menu slot="dropdown">
               <!-- 下拉选项 -->
               <el-dropdown-item>添加子部门</el-dropdown-item>
-              <el-dropdown-item>编辑部门</el-dropdown-item>
-              <el-dropdown-item>删除部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -37,6 +37,10 @@ export default {
     treeNode: {
       type: Object, // 对象类型
       required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
     }
   }
 }
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,68 +3,10 @@
     <div class="app-container">
       <!-- 组织架构内容 - 头部 -->
       <el-card class="tree-card">
-        <!-- 放置结构内容 -->
-        <!-- 作用域插槽slot-scope="obj" 接收传递给插槽地数据 data每个节点的数据对象 -->
-        <el-row type="flex" justify="space-between" align="middle" style="heigth: 40px" weight="100%">
-          <el-col>
-            <!-- 左侧内容 -->
-            <span>诚赢共创有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <!-- 内容 -->
-                  <span>操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 具名插槽 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <!-- 下拉选项 -->
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" :is-toot="true" />
         <!-- 放置一个el-tree  -->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-            <el-col>
-              <!-- 左侧内容 -->
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 放置下拉菜单 -->
-                  <el-dropdown>
-                    <!-- 内容 -->
-                    <span>操作
-                      <i class="el-icon-arrow-down" />
-                    </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-
-            <!-- 右侧内容 -->
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -72,9 +14,14 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      company: { name: '诚赢共创有限公司', manager: '负责人' },
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
         { name: '行政部', manager: '刘备' },
         { name: '人事部', manager: '孙权' }],
