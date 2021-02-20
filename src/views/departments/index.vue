70f66ead716179af4c94f750142977ef81efd58d
童永泽 <604232797@qq.com>
2021-02-20 19:41:55 +0800

    搭建头部

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 组织架构内容 - 头部 -->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <el-row type="flex" justify="space-between">
+          <el-col>
+            <!-- 左侧内容 -->
+            <span>诚赢共创有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <!-- 右侧内容 -->
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 放置下拉菜单 -->
+                <el-dropdown>
+                  <!-- 内容 -->
+                  <span>操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 具名插槽 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <!-- 下拉选项 -->
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
@@ -14,6 +41,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .tree-card {
+    padding: 30px 140px;
+    font-size: 14px;
+  }
 </style>
