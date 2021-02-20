fa0ba6eec76d47539f4e6cf3087c64ee5bfe929b
童永泽 <604232797@qq.com>
2021-02-20 21:40:58 +0800

    单独封装操作栏组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <el-col>
+      <!-- 左侧内容 -->
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 放置下拉菜单 -->
+          <el-dropdown>
+            <!-- 内容 -->
+            <span>操作
+              <i class="el-icon-arrow-down" />
+            </span>
+            <!-- 具名插槽 -->
+            <el-dropdown-menu slot="dropdown">
+              <!-- 下拉选项 -->
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item>编辑部门</el-dropdown-item>
+              <el-dropdown-item>删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+
+      <!-- 右侧内容 -->
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    //   定义一个props属性
+    treeNode: {
+      type: Object, // 对象类型
+      required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
