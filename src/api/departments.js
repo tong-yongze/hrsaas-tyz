3fafc2c5eec1155803beb712c54faccb9f56de8c
童永泽 <604232797@qq.com>
2021-02-21 19:38:25 +0800

    封装删除接口

--- src/api/departments.js
+++ src/api/departments.js
@@ -1,8 +1,21 @@
-// 获取组织架构数据
+
 import request from '@/utils/request'
 
+/**
+ * 获取组织架构的数据
+ * **/
 export function getDepartments() {
   return request({
     url: '/company/department'
+  })
+}
+
+/**
+ * 删除组织架构的数据
+ * **/
+export function delDepartment(id) {
+  return request({
+    url: ` /company/department/${id}`,
+    method: 'Delete'
   })
 }
