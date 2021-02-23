b33f8b7e079eb48c1b53fe28da2cc7d96ba7b024
童永泽 <604232797@qq.com>
2021-02-23 22:00:58 +0800

    角色基本结构完成

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,9 +1,52 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        考勤
-      </h2>
+      <!-- 放置内容 -->
+      <el-card>
+        <el-tabs>
+          <el-tab-pane label="角色管理">
+            <!-- 左侧的内容 -->
+            <el-row style="height: 60px">
+              <el-button type="primary" icon="el-icon-plus" size="small">新增角色</el-button>
+            </el-row>
+            <el-table border="">
+              <el-table-column label="序号" width="120px" />
+              <el-table-column label="名称" width="240px" />
+              <el-table-column label="描述" />
+              <el-table-column label="操作">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+
+            <!-- 放置分页组件 -->
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <el-pagination
+                layout="prev, pager, next"
+              />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert :closable="false" :show-icon="true" type="info" title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改" />
+            <!-- 右边内容 -->
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input type="textarea" :rows="3" disabled style="width: 400px" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
