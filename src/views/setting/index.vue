15d1e3cb57d5ef59b6278fdf932b635c9d44037f
童永泽 <604232797@qq.com>
2021-02-24 21:31:36 +0800

    读取公司信息

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -34,18 +34,22 @@
           <el-tab-pane label="公司信息">
             <el-alert :closable="false" :show-icon="true" type="info" title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改" />
             <!-- 右边内容 -->
-            <el-form label-width="120px" style="margin-top: 50px">
+            <!-- 并不是所有的表单都需要model rules -->
+            <el-form label-width="120px" style="margin-top:50px">
               <el-form-item label="公司名称">
-                <el-input disabled style="width: 400px" />
+                <el-input v-model="formData.name" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="公司地址">
-                <el-input disabled style="width: 400px" />
+                <el-input v-model="formData.companyAddress" disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="电话">
+                <el-input v-model="formData.companyPhone" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="邮箱">
-                <el-input disabled style="width: 400px" />
+                <el-input v-model="formData.mailbox" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="备注">
-                <el-input type="textarea" :rows="3" disabled style="width: 400px" />
+                <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="width:400px" />
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -56,7 +60,8 @@
 </template>
 
 <script>
-import { getRoleList } from '@/api/setting'
+import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { mapGetters } from 'vuex'
 
 export default {
   data() {
@@ -67,17 +72,27 @@ export default {
         page: 1,
         pagesize: 10,
         total: 0
+      },
+      formData: {
+        // 公司信息
       }
     }
   },
+  computed: {
+    ...mapGetters(['companyId'])
+  },
   created() {
     this.getRoleList() // 获取角色列表
+    this.getCompanyInfo()
   },
   methods: {
     async getRoleList() {
       const { total, rows } = await getRoleList(this.page)
       this.page.total = total
       this.list = rows
+    },
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId)
     },
     changePage(newPage) {
       this.page.page = newPage
