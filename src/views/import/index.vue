1263f6c6a04a35a8832165778ea09ad102837466
童永泽 <604232797@qq.com>
2021-02-28 09:31:48 +0800

    导入时间函数

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -30,15 +30,31 @@ export default {
       var newArr = results.map(item => {
         var userInfo = {}
         Object.keys(item).forEach(key => {
-          userInfo[userRelations[key]] = item[key]
+          if (userRelations[key] === 'timeOfEntry' || userRelations[key] === 'correctionTime') {
+            // 后端接口 限制了 不能是字符串  要求转化时间类型
+            userInfo[userRelations[key]] = new Date(this.formatDate(item[key], '/')) // 只有这样 才能存入数据库
+          } else { userInfo[userRelations[key]] = item[key] }
         })
         return userInfo
       })
       await importEmployee(newArr) // 接收一个数组
       this.$message.success('导入excel成功')
       this.$router.back() // 回到上一个页面
+    },
+    // 转化excel 的日期格式
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     }
   }
+
 }
 </script>
 
