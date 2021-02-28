0579c62d8ddb7a81b7406d2d4af72eee1774ba32
童永泽 <604232797@qq.com>
2021-02-28 20:36:41 +0800

    复杂表头的导出

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -59,6 +59,8 @@
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees' // 引入员工的枚举对象
 import AddEmployee from './components/add-employee'
+import { formatDate } from '@/filters'
+
 export default {
   components: {
     AddEmployee
@@ -108,8 +110,8 @@ export default {
     },
     exportData() {
       const headers = {
-        '手机号': 'mobile',
         '姓名': 'username',
+        '手机号': 'mobile',
         '入职日期': 'timeOfEntry',
         '聘用形式': 'formOfEmployment',
         '转正日期': 'correctionTime',
@@ -124,9 +126,14 @@ export default {
         // 获取员工的接口 页码 每页条数
         const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
         const data = this.formatJson(headers, rows) // 返回的data就是 要导出的数据
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
         excel.export_json_to_excel({
           header: Object.keys(headers),
-          data
+          data,
+          filename: '员工资料表',
+          multiHeader, // 复杂表单
+          merges // 合并选项
         })
         // excel.export_json_to_excel({
         //   header: ['姓名', '工资'],
@@ -141,16 +148,24 @@ export default {
     //  [{}]  [[]]
     formatJson(headers, rows) {
       return rows.map(item => {
-        // item  是一个对象 {mobile： 12345，username: "李四"}
-        // ["手机号"，"姓名"，"入职日期"]
+        // item是一个对象  { mobile: 132111,username: '张三'  }
+        // ["手机号", "姓名", "入职日期" 。。]
         return Object.keys(headers).map(key => {
+          // 需要判断  字段
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            // 格式化日期
+            return formatDate(item[headers[key]])
+          } else if (headers[key] === 'formOfEmployment') {
+            const obj = EmployeeEnum.hireType.find(obj => obj.id === item[headers[key]])
+            return obj ? obj.value : '未知'
+          }
           return item[headers[key]]
         })
+        // ["132", '张三’， ‘’，‘’，‘’d]
       })
       // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
       // 需要处理时间格式问题
     }
-
   }
 }
 </script>
