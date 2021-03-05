e9c0da1f59c8d45fa2db211b3e12a27277e0449c
童永泽 <604232797@qq.com>
2021-03-05 21:42:30 +0800

    权限处理

--- src/permission.js
+++ src/permission.js
@@ -26,10 +26,20 @@ router.beforeEach(async(to, from, next) => {
       // 如果当前vuex 中有用户的资料的 id 表示已经有资料了 不需要获取了 如果没有id才需要获取
       if (!store.getters.userId) {
         // 如果没有id 才表示当前用户资料没有获取过
-        await store.dispatch('user/getUserInfo')
+        const { roles } = await store.dispatch('user/getUserInfo')
         // 如果说后续 需要根据用户资料获取数据的话 这里需要改成 同步
+        // 筛选用户的可用路由
+        const routes = await store.dispatch('permission/filterRoutes', roles.menus) // 筛选得到当前用户可用的动态路由
+        // routes 就是筛选得到的动态路由
+        // 动态路由 添加到路由表中 默认的路由表 只有静态 没有动态
+        // addRoutes  必须用 next(地址) 不能用next()
+        // console.log(routes)
+        router.addRoutes([...routes, { path: '*', redirect: '/404', hidden: true }]) // 添加动态路由到路由表
+        // 添加完动态路由后
+        next(to.path) // 相当于跳到对应的地址  相当于多做一次跳转
+      } else {
+        next()
       }
-      next()
     }
   } else {
     // 如果木有token 情况下
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,6 @@
 import { getToken, setToken, removeToken, setTimeStamp } from '@/utils/auth'
 import { login, getUserInfo, getUserDetailById } from '@/api/user'
+import { resetRouter } from '@/router'
 // 状态
 const state = {
   token: getToken(), // 设置token 为共享状态 初始化vuex 的时候 就先从缓存中读取
@@ -43,12 +44,21 @@ const actions = {
     context.commit('setUserInfo', { ...result, ...baseInfo }) // 提交到 mutations
     return result // 这里为什么要返回 为后期做权限留下伏笔
   },
-  // 登出操作
+  // 登出的action
   logout(context) {
-  // 删除token
-    context.commit('removeToken')
-    // 清空用户资料
-    context.commit('removeUserInfo')
+    // 删除token
+    context.commit('removeToken') // 不仅仅删除了vuex中的 还删除了缓存中的
+    // 删除用户资料
+    context.commit('removeUserInfo') // 删除用户信息
+    // 重置路由
+    resetRouter()
+    // 还有一步  vuex中的数据是不是还在
+    // 要清空permission模块下的state数据
+    // vuex中 user子模块  permission子模块
+    // 子模块调用子模块的action  默认情况下 子模块的context是子模块的
+    // 父模块 调用 子模块的action
+    context.commit('permission/setRoutes', [], { root: true })
+    // 子模块调用子模块的action 可以 将 commit的第三个参数 设置成  { root: true } 就表示当前的context不是子模块了 而是父模块
   }
 }
 
